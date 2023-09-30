3c07d34ad8f1100c74b244519f7a491bdc15c789
Richard Domingo <chardskarth@gmail.com>
2023-09-30 14:49:57 +0800

    chore!: i use ``` in vimwiki

--- lua/defaults.lua
+++ lua/defaults.lua
@@ -2,7 +2,7 @@ local M = {}
 local lang_conf = {}
 
 lang_conf["markdown"] = { "```", "```" }
-lang_conf["vimwiki"] = { "{{{", "}}}" }
+lang_conf["vimwiki"] = { "```", "```" }
 lang_conf["norg"] = { "@code", "@end" }
 lang_conf["org"] = { "#+BEGIN_SRC", "#+END_SRC" }
 lang_conf["markdown.pandoc"] = { "```", "```" }
